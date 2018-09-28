7f52003cd27fcd828a72b934763a9bac5e3a0bff
Angelos Pikoulas <agelos.pikoulas@gmail.com>
2018-09-28 10:00:02 +0100

    feat(): import specHelpers from uberscore

--- src/it-works.spec.ts
+++ src/it-works.spec.ts
@@ -1,9 +1,15 @@
 import { Expect, Test, TestFixture } from 'alsatian';
+import { equalSet } from './specHelpers';
 
 @TestFixture()
 export class ItWorksSpec {
   @Test('itWorks')
   itWorks() {
     Expect(42).toBe(42);
+  }
+  
+  @Test('itUsesSpecHelpers')
+  itUsesSpecHelpers() {
+    equalSet([1,2,3], [3,2,1,4])
   }
 }
